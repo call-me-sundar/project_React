787d4534c09dea8b24286aaf5c42fc9de18d1811
Sundar <sundar10072001@gmail.com>
2023-09-01 15:08:08 +0530

    new chnages

--- controllers/taskcontrollers.js
+++ controllers/taskcontrollers.js
@@ -2,7 +2,7 @@ const db = require('../models/db')
 
 const getAllData = async (req, res) => {
     try {
-        const [result] = await db.query("select * from tabledatas");
+        const [result] = await db.query("select * from dashboardtable");
         res.status(200).json(result)
     } catch (err) {
         res.status(400).json("error", err)
